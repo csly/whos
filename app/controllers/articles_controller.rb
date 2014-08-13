50ce8180d54c1bdda737234e882ea69900ba3aed
csly <carasly@me.com>
2014-08-13 14:15:25 +0100

    Featured with tags

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,71 @@
+class ArticlesController < ApplicationController
+	include ArticlesHelper
+
+	http_basic_authenticate_with :name => "bucks", :password => "testing"
+
+	before_filter :require_login, only: [:new, :create, :edit, :update, :destroy]
+
+	def index
+		@articles = Article.featured_article + Article.unfeatured_article 
+
+  		 
+	end
+	def show
+		@article = Article.find(params[:id])
+	end
+	def new
+		@article = Article.new
+
+	end
+	def create
+    	@article = Article.new(article_params)
+   		@article.save
+
+   		flash.notice = "Article '#{@article.title}' has been created!"
+
+   		redirect_to article_path(@article)
+  	end
+  	def edit
+ 		@article = Article.find(params[:id])
+	end
+	def destroy
+ 		@article = Article.find(params[:id])
+ 		@article.destroy
+
+ 		flash.notice = "Article '#{@article.title}' has been deleted!"
+
+ 		redirect_to articles_path
+
+	end
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		flash.notice = "Article '#{@article.title}' Updated!"
+
+		redirect_to article_path(@article)
+	end
+	def tagged
+ 		 if params[:tag].present? 
+   		 @articles = Article.tagged_with(params[:tag])
+ 		 else 
+   		 @articles = Article.all
+  		end  
+	end
+
+	 def featured
+ 		 if params[:featured].present? 
+   		 @articles = Article.find(params[:featured])
+ 		 else 
+   		 @articles = Article.all
+  		end  
+	end
+  
+ 
+	  
+
+
+
+
+	 
+end
---
+++ app/helpers/articles_helper.rb
@@ -0,0 +1,8 @@
+module ArticlesHelper
+include ActsAsTaggableOn::TagsHelper
+
+	def article_params
+		params.require(:article).permit(:title, :body, :biog, :dead_line, :label, :website, :facebook, :twitter, :tag_list,   :featured, :image)
+	end
+	
+end
